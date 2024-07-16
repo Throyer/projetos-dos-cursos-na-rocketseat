e12b5f4123548625c535375429ae833a9ee64fcf
Renato Henrique <renato.henrique_98@hotmail.com>
2024-07-15 23:22:45 -0300

    add syntax-highlighter

--- reactjs/blog-post/src/components/content/content.tsx
+++ reactjs/blog-post/src/components/content/content.tsx
@@ -1,15 +1,36 @@
-import css from "./content.module.css"
+import Markdown from 'react-markdown';
 
-import Markdown from 'react-markdown'
+import { PrismLight } from 'react-syntax-highlighter';
 
-import { createStyles } from "@utils/css-modules"
+import { oneDark } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+
+import css from "./content.module.css";
+
+import { createStyles } from "@utils/css-modules";
 
 const styles = createStyles(css);
 
 export const Content = ({ content }: { content: string }) => {
+  css.content
   return (
     <div className={styles('content')}>
-      <Markdown>
+      <Markdown components={{ code({ node, className, ...props }) {
+        const lang = /language-(\w+)/.exec(className || '');
+            
+        return lang ? (
+          <PrismLight
+            style={oneDark}
+            language={lang[1]}
+            className="codeStyle"
+            showLineNumbers={true}
+            useInlineStyles={true}
+          >
+            {String(props.children).replace(/\n$/, '')}
+          </PrismLight>
+        ) : (
+          <code className={className} {...props} />
+        )
+      }}}>
         {content}
       </Markdown>
     </div>
