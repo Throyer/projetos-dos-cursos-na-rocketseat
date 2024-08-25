9092a90a0e03ebc726c464b1841d400b396fcfe3
Renato Henrique <renato.henrique_98@hotmail.com>
2024-08-25 14:18:01 -0300

    create timer

--- reactjs/ignite-timer/src/pages/home/index.tsx
+++ reactjs/ignite-timer/src/pages/home/index.tsx
@@ -1,5 +1,35 @@
+import { Play } from "@phosphor-icons/react"
+
+import * as Styles from "./styles";
+
 export const Home = () => {
   return (
-    <h1>Home wokrs!</h1>
+    <Styles.Container>
+      <form>
+        <Styles.Inputs>
+
+          <label htmlFor="task">Vou trabalhar em</label>
+          <input id="task" />
+
+          <label htmlFor="minutesAmount">durante</label>
+          <input type="number" id="minutesAmount" />
+
+          <span>minutos.</span>
+        </Styles.Inputs>
+
+        <Styles.Countdown>
+          <span>0</span>
+          <span>0</span>
+          <Styles.Separator>:</Styles.Separator>
+          <span>0</span>
+          <span>0</span>
+        </Styles.Countdown>
+
+        <button type="submit">
+          <Play size={24} />
+          começar
+        </button>
+      </form>
+    </Styles.Container>
   )
 }
