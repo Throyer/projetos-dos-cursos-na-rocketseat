86e0af25ce9a3713fea68911017b22c9bb8b8544
Renato Henrique <renato.henrique_98@hotmail.com>
2024-08-25 22:51:45 -0300

    add table

--- reactjs/ignite-timer/src/pages/history/index.tsx
+++ reactjs/ignite-timer/src/pages/history/index.tsx
@@ -1,5 +1,44 @@
 import * as Styles from "./styles"
 
+const Statuses = {
+  'Concluído': 'green',
+  'Em andamento': 'yellow',
+  'Pendente': 'red'
+} as const
+
+const FAKE_ITENS = [
+  {
+    "id": 1,
+    "title": "Tarefa 1",
+    "started_at": "Há cerca de duas horas",
+    "status": "Concluído"
+  },
+  {
+    "id": 2,
+    "title": "Tarefa 2",
+    "started_at": "Há cerca de três horas",
+    "status": "Em andamento"
+  },
+  {
+    "id": 3,
+    "title": "Tarefa 3",
+    "started_at": "Há cerca de uma hora",
+    "status": "Pendente"
+  },
+  {
+    "id": 4,
+    "title": "Tarefa 4",
+    "started_at": "Há cerca de duas horas",
+    "status": "Concluído"
+  },
+  {
+    "id": 5,
+    "title": "Tarefa 5",
+    "started_at": "Há cerca de três horas",
+    "status": "Em andamento"
+  },
+];
+
 export const History = () => {
   return (
     <Styles.Container>
@@ -7,7 +46,30 @@ export const History = () => {
 
       <Styles.Content>
         <table>
-          
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {FAKE_ITENS.map(({ id, title, started_at, status }) => (
+              <tr key={id}>
+                <td>{title}</td>
+                <td>20 minutos</td>
+                <td>{started_at}</td>
+                <td>
+                  <Styles.Status
+                    statusColor={Statuses[status as keyof typeof Statuses]}
+                  >
+                    {status}
+                  </Styles.Status>
+                </td>
+              </tr>
+            ))}
+          </tbody>
         </table>
       </Styles.Content>
     </Styles.Container>
--- reactjs/ignite-timer/src/pages/history/styles.ts
+++ reactjs/ignite-timer/src/pages/history/styles.ts
@@ -17,4 +17,103 @@ export const Content = styled.div`
   flex: 1;
   overflow: auto;
   margin-top: 2rem;
+  overflow-y: scroll;
+  max-height: 23rem;
+
+  border-radius: 8px;
+
+  &::-webkit-scrollbar {
+    width: 0.374rem;
+    height: 12.625rem;
+    cursor: pointer;
+  }
+
+  &::-webkit-scrollbar:horizontal {
+    width: 12.625rem;
+    height: 0.374rem;
+    cursor: pointer;
+  }
+
+  &::-webkit-scrollbar-track {
+    box-shadow: inset 0 0 5px transparent;
+    border-radius: 10px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background: ${(props) => props.theme['gray-400']};
+    border-radius: 10px;
+  }
+
+  table {
+    position: relative;
+    width: 100%;
+    border-collapse: collapse;
+    min-width: 600px;
+
+    th {
+      background-color: ${({ theme }) => theme['gray-600']};
+      padding: 1rem;
+      text-align: left;
+      color: ${({ theme }) => theme['gray-100']};
+      font-size: 0.875rem;
+      line-height: 1.6;
+
+      position: sticky;
+      top: 0;
+      z-index: 1;
+
+      &:first-child {
+        border-top-left-radius: 8px;
+        padding-left: 1.5rem;
+      }
+
+      &:last-child {
+        border-top-right-radius: 8px;
+        padding-right: 1.5rem;
+      }
+    }
+
+    td {
+      background-color: ${({ theme }) => theme['gray-700']};
+      border-top: 4px solid ${({ theme }) => theme['gray-800']};
+      padding: 1rem;
+      font-size: 0.875rem;
+      line-height: 1.6;
+
+      &:first-child {
+        width: 50%;
+        padding-left: 1.5rem;
+      }
+
+      &:last-child {
+        padding-right: 1.5rem;
+      }
+    }
+  }
 `;
+
+const ColorOptions = {
+  yellow: 'yellow-500',
+  red: 'red-500',
+  green: 'green-500',
+} as const
+
+type StatusProps = {
+  statusColor?: keyof typeof ColorOptions
+}
+
+export const Status = styled.span<StatusProps>`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+
+  &::before {
+    content: '';
+    width: 0.5rem;
+    height: 0.5rem;
+    border-radius: 50%;
+
+    background: ${({ theme, statusColor }) =>
+      theme[ColorOptions[statusColor || 'green']]};
+  }
+`
