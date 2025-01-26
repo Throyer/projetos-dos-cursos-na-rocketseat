177cc516ee27ff501c8845d797f6eff694535a46
Renato Henrique <renato.henrique_98@hotmail.com>
2025-01-26 20:07:56 -0300

    fala dev frescura

--- reactjs/ignite-timer/src/contexts/cycles/index.tsx
+++ reactjs/ignite-timer/src/contexts/cycles/index.tsx
@@ -28,54 +28,53 @@ export const CyclesProvider = ({ children }: PropsWithChildren) => {
   const [{ cycles, currentCycleId }, dispatch] = useReducer((state: CycleState, action: CycleAction<Cycle>) => {
     const { type, payload } = action;
 
-    if (type === 'add') {
-      return {
-        ...state,
-        cycles: [...state.cycles, payload.data],
-        currentCycleId: payload.data.id
-      };
+    switch (type) {
+      case 'add':
+        return {
+          ...state,
+          cycles: [...state.cycles, payload.data],
+          currentCycleId: payload.data.id
+        };
+
+      case 'stop':
+        return {
+          ...state,
+          cycles: state.cycles.map(cycle => {
+            if (cycle.id !== state.currentCycleId) {
+              return cycle;
+            }
+
+            cycle.status = 'Interrompido';
+            cycle.interrupted_at = new Date().toJSON();
+
+            return cycle;
+          }),
+          currentCycleId: null
+        };
+
+      case 'finished':
+        return {
+          ...state,
+          cycles: state.cycles.map(cycle => {
+            if (cycle.id !== state.currentCycleId) {
+              return cycle;
+            }
+
+            cycle.status = 'Concluído';
+            cycle.finished_at = new Date().toJSON();
+
+            return cycle;
+          }),
+          currentCycleId: null
+        };
     }
 
-    if (type === 'stop') {
-      return {
-        ...state,
-        cycles: state.cycles.map(cycle => {
-          if (cycle.id !== state.currentCycleId) {
-            return cycle;
-          }
-  
-          cycle.status = 'Interrompido';
-          cycle.interrupted_at = new Date().toJSON();
-  
-          return cycle;
-        }),
-        currentCycleId: null
-      };
-    }
-
-    if (type === 'finished') {
-      return {
-        ...state,
-        cycles: state.cycles.map(cycle => {
-          if (cycle.id !== state.currentCycleId) {
-            return cycle;
-          }
-  
-          cycle.status = 'Concluído';
-          cycle.finished_at = new Date().toJSON();
-  
-          return cycle;
-        }),
-        currentCycleId: null
-      };
-    }
-    
     return state;
   },
-  { 
-    cycles: [],
-    currentCycleId: null
-  });
+    {
+      cycles: [],
+      currentCycleId: null
+    });
 
   const currentCycle = cycles.find(cycle => cycle.id === currentCycleId);
 
